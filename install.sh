8cde182ed43838f388d30490033f2af12bdd31af
tyler <happydooby@gmail.com>
2016-05-18 19:18:53 -0700

    Reorganizing

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ng build -prod
 cd ..
 
 # Building the back end
+cd chat-practice-service
 mvn clean install
 pkill -9 -f target/chat-websocket.jar
 nohup java -jar target/chat-websocket.jar &> log.out &
