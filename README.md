fc522212405737efd2b38ee4c627032ba303c13f
Dessalines <tyhou13@gmx.com>
2016-05-24 12:59:39 -0700

    Got comment_breadcrumbs mostly working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chat-websocket
+http://stackoverflow.com/questions/8252323/mysql-closure-table-hierarchical-database-how-to-pull-information-out-in-the-c
+http://stackoverflow.com/questions/192220/what-is-the-most-efficient-elegant-way-to-parse-a-flat-table-into-a-tree/
---
+++ chat-practice-service/src/main/java/com/chat/db/Tables.java
@@ -0,0 +1,22 @@
+package com.chat.db;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+/**
+ * Created by tyler on 5/24/16.
+ */
+public class Tables {
+
+    @Table("user_")
+    public static class User extends Model {}
+    public static final User USER = new User();
+
+    @Table("comment")
+    public static class Comment extends Model {}
+    public static final Comment COMMENT = new Comment();
+
+    @Table("comment_tree")
+    public static class CommentTree extends Model {}
+    public static final CommentTree COMMENT_TREE = new CommentTree();
+}
