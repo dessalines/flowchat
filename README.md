114e8f130945327df99d40181657c3d37586833a
Dessalines <tyhou13@gmx.com>
2016-06-07 19:32:22 -0700

    Recursive comment finding and adding is mostly working

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ http://stackoverflow.com/questions/192220/what-is-the-most-efficient-elegant-way
   - Change them to a new highlighted color, then remove that.
   - Should you scroll when you are currently replying, or wait till after, or not at all?
   - http://stackoverflow.com/questions/34522306/angular-2-focus-on-newly-added-input-element
+  - Recursion: [1](http://stackoverflow.com/a/2549333/1655478) [2](http://stackoverflow.com/questions/16228467/how-do-i-break-out-of-loops-in-recursive-functions)
 - Reconnect websocket if disconnected
   - http://stackoverflow.com/questions/3479734/javascript-jquery-test-if-window-has-focus
   
--- ui/src/app/chat/chat.component.ts
+++ ui/src/app/chat/chat.component.ts
@@ -7,129 +7,92 @@ import {CommentComponent} from '../comment';
 
 
 @Component({
-    moduleId: module.id,
-    selector: 'app-chat',
-    templateUrl: 'chat.component.html',
-    styleUrls: ['chat.component.css'],
-    providers: [HTTP_PROVIDERS, ThreadedChatService],
-    directives: [CommentComponent]
+  moduleId: module.id,
+  selector: 'app-chat',
+  templateUrl: 'chat.component.html',
+  styleUrls: ['chat.component.css'],
+  providers: [HTTP_PROVIDERS, ThreadedChatService],
+  directives: [CommentComponent]
 })
 export class ChatComponent implements OnInit {
 
-    private comments: Array<Comment>;
-    private users: Array<string> = [];
-    private newCommentId: number;
+  private comments: Array<Comment>;
+  private users: Array<string> = [];
+  private newCommentId: number;
 
-    constructor(private threadedChatService: ThreadedChatService) {
+  private recursiveCommentStopper: boolean = false;
 
-        this.threadedChatService.ws.getDataStream().subscribe(res => {
-            this.updateThreadedChat(res.data);
-        });
+  constructor(private threadedChatService: ThreadedChatService) {
 
-
-    }
-
-    ngOnInit() { }
-
-
-    // TODO only send specifically added comment object
-    // alter the comments by searching through them, and only changing the specific one
-    // use push, set,etc. Don't replace all the comments
-
-    // The replying mode should be event emitters pushed up to this top component,
-    // not the specific one
-    updateThreadedChat(someData) {
-        let data = JSON.parse(someData);
-        console.log(data);
-
-        if (data.comments) {
-            this.comments = data.comments;
-        }
-
-        if (data.users) {
-            this.users = data.users;
-        }
-
-        if (data.id) {
-            console.log('its only new comment');
-
-            let newComment: Comment = data;
-
-            // Gotta basically place this in the correct location in the array of comments
-            this.addNewComment(newComment);
-        }
-
-    }
-
-
-    // sample:
-    // Object { id: 44, userId: 197, discussionId: 1, parentId: 43, topParentId: 1, pathLength: 6, 
-    // numOfParents: 6, numOfChildren: 0, userName: "user_197", text: "fdszxcv", topParentId: 1,
-    // userId: 197, userName: "user_197"}
-
-    private addNewComment(newComment: Comment) {
-
-        // First test adding comment as a top level(to see if it rerenders everything :(
-        // this.comments.push(newComment); // checks out, it didn't rerender
-
-        // Next test adding it as one down
-        // this.comments[0].embedded.push(newComment); // checks out, it didn't render
-
-
-        // If its the top level, stop and return 
-        if (newComment.parentId == null) {
-            this.comments.push(newComment);
-            return;
-        }
-
-
-        // loop to find correct top level
-        let topLevel: Comment = this.comments.filter(item => item.id == newComment.topParentId)[0];
-
-        console.log("new comment id: " + newComment.id);
-        
-        let parent = topLevel; 
-
-        this.recursiveComment(newComment, parent);
-
-        // break and continue
-        // http://stackoverflow.com/a/2549333/1655478
-
-
-
-        // recursive loop to find immediate parent
-
-
-
-        // add the comment to the immediate parent
-
-        // force a reload of that immediate parent?
-
-    }
-
-  private recursiveComment(newComment: Comment, parent: Comment) {
-      for (let ) {
-
-
-          console.log("parent:");
-          console.log(parent);
-
-            if (parent.id == newComment.id) {
-              console.log("found the correct parent id");
-              parent.embedded.push(newComment);
-              break;
-          } else {
-              // loop over the embedded array
-              let level: Comment = parent.embedded.filter(item => item.id == newComment.parentId)[0];
-              console.log(level);
-              if (level == null) {
-
-              }
-          }
-
+    this.threadedChatService.ws.getDataStream().subscribe(res => {
+      this.updateThreadedChat(res.data);
+    });
 
 
   }
+
+  ngOnInit() { }
+
+
+  // TODO only send specifically added comment object
+  // alter the comments by searching through them, and only changing the specific one
+  // use push, set,etc. Don't replace all the comments
+
+  // The replying mode should be event emitters pushed up to this top component,
+  // not the specific one
+  updateThreadedChat(someData) {
+    let data = JSON.parse(someData);
+    console.log(data);
+
+    if (data.comments) {
+      this.comments = data.comments;
+    }
+
+    if (data.users) {
+      this.users = data.users;
+    }
+
+    if (data.id) {
+      console.log('its only new comment');
+
+      let newComment: Comment = data;
+
+      // Gotta basically place this in the correct location in the array of comments
+      this.addNewComment(newComment);
+    }
+
+  }
+
+  private addNewComment(newComment: Comment) {
+
+    // If its the top level, stop and return 
+    if (newComment.parentId == null) {
+      this.comments.push(newComment);
+      return;
+    }
+
+    // filter to find correct top level
+    let topLevel: Comment = this.comments.filter(item => item.id == newComment.topParentId)[0];
+
+    // Do a recursive loop to find and push the new comment
+    this.recursiveComment(newComment, topLevel);
+    this.recursiveCommentStopper = false;
+  }
+
+
+
+  private recursiveComment(newComment: Comment, parent: Comment) {
+
+    if (parent.id == newComment.parentId) {
+      parent.embedded.push(newComment);
+      this.recursiveCommentStopper = true;
+    } else {
+      for (let emb of parent.embedded) {
+        if (!this.recursiveCommentStopper) {
+          this.recursiveComment(newComment, emb);
+        }
+      }
+    }
   }
 
 }
