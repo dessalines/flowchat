1c8f5a97d82cc0e25fcaf84e0808928b756f8125
Dessalines <tyhou13@gmx.com>
2016-06-22 15:23:06 -0700

    Some styling, correct tag linking

--- ui/src/app/components/discussion-card/discussion-card.component.ts
+++ ui/src/app/components/discussion-card/discussion-card.component.ts
@@ -4,12 +4,14 @@ import { MomentPipe } from '../../pipes/moment.pipe';
 import {MarkdownPipe} from '../../pipes/markdown.pipe';
 import {UserService} from '../../services/user.service';
 import {DiscussionService} from '../../services/discussion.service';
+import { Router, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
 @Component({
   moduleId: module.id,
   selector: 'app-discussion-card',
   templateUrl: 'discussion-card.component.html',
   styleUrls: ['discussion-card.component.css'],
+  directives: [ROUTER_DIRECTIVES],
   pipes: [MomentPipe, MarkdownPipe]
 })
 export class DiscussionCardComponent implements OnInit {
@@ -19,7 +21,8 @@ export class DiscussionCardComponent implements OnInit {
   private showVoteSlider: boolean = false;
 
   constructor(private userService: UserService,
-    private discussionService:DiscussionService) {}
+    private discussionService:DiscussionService,
+    private router: Router) {}
 
   ngOnInit() {
   }
