51357e6d3b4a57a0f2694a5fb2ef159777e1280f
Dessalines <tyhou13@gmx.com>
2016-07-07 16:22:44 -0700

    Notifications working

--- ui/src/app/services/notifications.service.ts
+++ ui/src/app/services/notifications.service.ts
@@ -4,8 +4,8 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/mergeMap';
-
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 import {UserService} from './user.service';
@@ -17,21 +17,21 @@ export class NotificationsService {
   private getUnreadUrl: string = 'http://localhost:4567/get_unread_replies';
   private markAsReadUrl: string = 'http://localhost:4567/mark_reply_as_read/';
 
-  private fetchInterval: number = 10000;
+  private fetchInterval: number = 60000;
 
   constructor(private http: Http,
     private userService: UserService) {
   }
 
-  getUnreadComments() {
-    return Observable.interval(this.fetchInterval).flatMap(() => {
+  getUnreadMessages() {
+    return Observable.interval(this.fetchInterval).startWith(0).flatMap(() => {
       return this.http.get(this.getUnreadUrl, this.userService.getOptions())
         .map(this.extractData)
         .catch(this.handleError);
     });
   }
 
-  markReplyAsRead(id: string) {
+  markMessageAsRead(id: number) {
     return this.http.post(this.markAsReadUrl + id, null, this.userService.getOptions())
       .map(this.extractData)
       .catch(this.handleError);
