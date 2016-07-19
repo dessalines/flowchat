9e4c2b54ba4e4323858db277621b345215f86979
Tyler <happydooby@gmail.com>
2016-07-19 09:15:22 -0700

    Got emoji support working

--- ui/src/app/pipes/markdown.pipe.ts
+++ ui/src/app/pipes/markdown.pipe.ts
@@ -1,13 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as markdown_it from 'markdown-it';
 
+declare var markdownitEmoji: any;
+
 @Pipe({
   name: 'markdown'
 })
 export class MarkdownPipe implements PipeTransform {
 
+  private markdownIt: markdown_it.MarkdownIt;
+
+  constructor() {
+    this.markdownIt = markdown_it();
+    this.markdownIt.use(markdownitEmoji);
+  }
+
   transform(value: string): any {
-    return markdown_it().render(value);
+    return this.markdownIt.render(value);
   }
 
 }
