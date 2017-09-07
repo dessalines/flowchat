54106854c15b3e3a214013da25111c7b56b293b4
Tyler <happydooby@gmail.com>
2017-09-07 16:24:34 -0700

    Adding a few more deploy steps.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 if [ "$TRAVIS_BRANCH" == "master"  ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     export SSHPASS=$DEPLOY_PASS
-    sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '"' -f 4 | xargs wget; java -jar flowchat.jar"
+    sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "rm -f flowchat.jar; \
+    curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; \
+    pkill -f flowchat.jar; \
+    java -jar flowchat.jar"
+fi
