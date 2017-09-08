cc011f25f5fb9fe8319a87cd8805f2cd4c8e0551
Dessalines <tyhou13@gmx.com>
2017-09-07 21:21:14 -0700

    Removing the check from deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-if [ "$TRAVIS_BRANCH" == "master"  ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    export SSHPASS=$DEPLOY_PASS
-    sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "rm -f flowchat.jar; \
-    curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; \
-    pkill -f flowchat.jar; \
-    java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import"
-fi
+export SSHPASS=$DEPLOY_PASS
+sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "rm -f flowchat.jar; \
+curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; \
+pkill -f flowchat.jar; \
+java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import"
