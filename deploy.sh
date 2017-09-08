c2a3c18353a7d66a53b224499e5bd8cc09898a75
Dessalines <tyhou13@gmx.com>
2017-09-08 08:34:16 -0700

    Trying to fix deploy again.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 export SSHPASS=$DEPLOY_PASS
-sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "rm -f flowchat.jar; \
+sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "pkill -f java; \
+rm -f flowchat.jar; \
 curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; \
-pkill -f flowchat.jar; \
 unzip -o flowchat.jar -d /tmp/.flowchat.tmp; \
-java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import >> log.out &"
+nohup java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import >> log.out &"
