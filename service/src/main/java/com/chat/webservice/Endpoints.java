9ead55456fd2940a2f1df2700e297ce3bc547f04
Dessalines <tyhou13@gmx.com>
2017-09-08 07:26:30 -0700

    Trying deploy again.

--- service/src/main/java/com/chat/webservice/Endpoints.java
+++ service/src/main/java/com/chat/webservice/Endpoints.java
@@ -51,7 +51,7 @@ public class Endpoints {
         });
 
         after((req, res) -> {
-//            res.header("Content-Encoding", "gzip");
+            res.header("Content-Encoding", "gzip");
             Tools.dbClose();
         });
 
