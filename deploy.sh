4f6cd9db9882fe47c13f3b2fe0a11ee550cf8b1e
Tyler <happydooby@gmail.com>
2017-09-08 08:57:45 -0700

    Again.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 export SSHPASS=$DEPLOY_PASS
-sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "pkill -f java; \
-rm -f flowchat.jar; \
-curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; \
-unzip -o flowchat.jar -d /tmp/.flowchat.tmp; \
-nohup java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import >> log.out &"
+sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "rm -f flowchat.jar; curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; unzip -o flowchat.jar -d /tmp/.flowchat.tmp; java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import >> log.out &"
