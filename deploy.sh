ae48f4e1167d117752e313e32cc2f79851cfe6f1
Tyler <happydooby@gmail.com>
2017-09-07 20:54:54 -0700

    Moving environment stuff out of pom.xml. Fixes #111.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ if [ "$TRAVIS_BRANCH" == "master"  ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; t
     sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "rm -f flowchat.jar; \
     curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; \
     pkill -f flowchat.jar; \
-    java -jar flowchat.jar"
+    java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import"
 fi
--- service/install.sh
+++ service/install.sh
@@ -1,3 +1,4 @@
 mvn clean install
 pkill -9 -f target/flowchat.jar
+echo -e 'jdbc.url=jdbc\:postgresql\://127.0.0.1/flowchat\njdbc.username=flowchat\njdbc.password=asdf\nsorting_created_weight=3600\nsorting_number_of_votes_weight=0.001\nsorting_avg_rank_weight=0.01\nreddit_client_id=\nreddit_client_secret=\nreddit_username=\nreddit_password='>flowchat.properties
 nohup java -jar target/flowchat.jar $@ >> log.out &
