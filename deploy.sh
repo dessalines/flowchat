edfd8e1537769e718ef28df7aaa894ebea30f95d
Dessalines <tyhou13@gmx.com>
2017-09-08 07:46:57 -0700

    Try removing nohup.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "rm -f flow
 curl -s https://api.github.com/repos/dessalines/flowchat/releases/latest | grep browser_download_url | cut -d '\"' -f 4 | xargs wget; \
 pkill -f flowchat.jar; \
 unzip -o flowchat.jar -d /tmp/.flowchat.tmp; \
-nohup java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import >> log.out &"
+java -jar flowchat.jar -ssl ~/keystore.jks -reddit_import >> log.out &"
