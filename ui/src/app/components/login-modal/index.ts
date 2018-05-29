baf12930dd62927b72ffe042cd90c91dc6b527e2
Tyler <happydooby@gmail.com>
2018-05-29 12:49:17 -0700

    Adding some modals and forgotten angular files.

---
+++ ui/src/app/components/login-modal/index.ts
@@ -0,0 +1,1 @@
+export { LoginModalComponent } from './login-modal.component';
---
+++ ui/src/app/components/login-modal/login-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginModalComponent } from './login-modal.component';
+
+describe('LoginModalComponent', () => {
+  let component: LoginModalComponent;
+  let fixture: ComponentFixture<LoginModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginModalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
